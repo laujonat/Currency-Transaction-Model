15069f3fb90851cf0d7b498b0f8de606051f8b5f
Jonathan Lau <jonhlau28@gmail.com>
2018-06-30 16:20:59 -0700

    configuration file not working

---
+++ __tests__/components/CurrencyWidget.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store'; // Smart components
+
+import CurrencyWidget from '../../src/components/CurrencyWidget';
+
+
+describe('<CurrencyWidget />', () => {
+  describe('render()', () => {
+    test('renders currency widget', () => {
+      const wrapper = shallow(<CurrencyWidget />);
+      // const component = wrapper.dive();
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+
+});
