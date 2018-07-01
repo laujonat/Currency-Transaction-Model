f2b2ff5401e601b2f260974ec0830d8e342f005a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-06-30 17:06:59 -0700

    Setup Complete (hopefully)

---
+++ src/components/CurrencyWidget.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CurrencyWidget extends Component {
+  constructor() {
+    super();
+    this.hi = "hi";
+  }
+
+  render() {
+    return (<div>"hi"</div>);
+  }
+}
