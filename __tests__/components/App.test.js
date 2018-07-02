79c13eceba464944bcf48faf80cddd1c19d27ed6
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-07-01 17:07:04 -0700

    initial app test

--- __tests__/components/App.test.js
+++ __tests__/components/App.test.js
@@ -3,16 +3,17 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store'; // Smart components
 
+import App from '../../src/components/app';
 import CurrencyWidget from '../../src/components/CurrencyWidget';
 
 
-describe('<CurrencyWidget />', () => {
+
+describe('<App />', () => {
   describe('render()', () => {
-    test('renders currency widget', () => {
-      const wrapper = shallow(<CurrencyWidget />);
+    test('renders two currency widgets', () => {
+      const wrapper = shallow(<App />);
       console.log(wrapper);
-      expect(wrapper.exists().to.equal(false));
+      expect(wrapper.find(CurrencyWidget).to.have.length(2));
     });
   });
-
 });
