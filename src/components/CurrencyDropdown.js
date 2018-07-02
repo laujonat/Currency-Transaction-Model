fc3c071e20e9a65419094af49edd8e794d610f81
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-07-02 15:07:37 -0700

    Update dropdown

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 
 const dropdown = function({ currencies }) {
   const currObj = Object.keys(currencies).sort();
-  return currObj.map((curr, idx) => <option key={idx} value={currencies[curr]}>{curr}</option>);
+  
+  return currObj.map((curr, idx) => {
+    return <option key={idx} value={currencies[curr]}>{curr}</option>;
+  });
 };
-
 
 const mapStateToProps = state => ({
   currencies: state.currencies.curr,
