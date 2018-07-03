94e0b8d6d6b72aee7f377e767cda081883a5ebc3
Jonathan Lau <jonhlau28@gmail.com>
2018-07-02 17:11:36 -0700

    user slice of state

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import currencyReducer from './currencyReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
+  users: userReducer,
   currencies: currencyReducer
 });
 
