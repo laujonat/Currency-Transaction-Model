313730c0309f0784df8d0fdcbbf8fc23b9fe39d6
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-07-03 22:20:11 -0700

    Comment testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+
 import Root from './components/root';
 import rootReducer from './reducers/rootReducer';
 import { fetchCurrencies } from "./actions/curr_actions";
@@ -18,9 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
   );
   store.dispatch(fetchCurrencies());
   store.dispatch(fillUserWallet());
+
+  // TESTING
   window.fetchCurrencies = fetchCurrencies;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  // TESTING
 
   const root = document.getElementById('converter');
   ReactDOM.render(<Root store={ store }/>, root);
