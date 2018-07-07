deb1f261c7a5228036978cf56b08daeb8ebed259
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-07-06 20:54:24 -0500

    Start history reducer

---
+++ src/reducers/historyReducer.js
@@ -0,0 +1,16 @@
+// import { } from '../actions/history_actions'; // does not exist
+
+const initialState = {
+
+};
+
+const historyReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'Nameless':
+    default:
+      return state;
+  }
+};
+
+
+export default historyReducer;
