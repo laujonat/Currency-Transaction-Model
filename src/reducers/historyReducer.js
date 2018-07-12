a2039964c2bfb16d972333ca3dc027268e049804
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-07-12 12:26:24 -0700

    Add entry to history when send funds

--- src/reducers/historyReducer.js
+++ src/reducers/historyReducer.js
@@ -1,16 +1,33 @@
 // import { } from '../actions/history_actions'; // does not exist
+import {
+  SEND_USER_FUNDS
+} from '../actions/user_actions';
 
-const initialState = {
-
-};
+const initialState = [
+  {
+    from: 'userA',
+    to: 'userB',
+    amount: 100
+  }
+];
 
 const historyReducer = (state = initialState, action) => {
+  let newState;
+
   switch(action.type) {
-    case 'Nameless':
+    case SEND_USER_FUNDS:
+      newState = state;
+      const transaction = {
+        from: action.fromUser,
+        to: action.toUser,
+        amount: action.amt
+      };
+
+      newState.push(transaction);
+      return newState;
     default:
       return state;
   }
 };
-
 
 export default historyReducer;
